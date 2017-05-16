86209ab52483a17710c3183b223a1cb88fd18e40
Elizabeth Reiher <tonomoshia@gmail.com>
2017-05-16 13:25:06 -0400

    Age Calculator (after walkthrough) accounting for leap years

--- 3_Javascript/3_Age_Calculator/age.js
+++ 3_Javascript/3_Age_Calculator/age.js
@@ -1,5 +1,5 @@
 var age = prompt("How old are you?");
-var days = age * 365;
+// accounting for leap years
+var days = age * 365.25;
 alert(age + " years is roughly " + days + " days.");
-
 console.log(age + " years is roughly " + days + " days.");
