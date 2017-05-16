35544e1d1c60cbe6dec6c5238528d04e8fedbd54
Elizabeth Reiher <tonomoshia@gmail.com>
2017-05-15 23:58:46 -0400

    JS Stalker Exercise
    Asks for user's first name, last name and age. I added a bootstrap CDN and my own CSS to make it look how I wanted. I also had it print out to an alert *and* to the console. In his exercise, he just wants you to print to the console.

---
+++ 3_Javascript/2_JS_Stalker_Exercise/stalker.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+alert("Your full name is " + firstName + " " + lastName + ".");
+alert("Your age is " + age + ".");
+console.log("Your full name is " + firstName + " " + lastName + ".");
+console.log("Your age is " + age + ".");
