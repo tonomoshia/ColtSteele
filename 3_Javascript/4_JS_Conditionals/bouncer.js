05a9cb6245a5d438fe3cd04edb39fe5b82d345b2
Elizabeth Reiher <tonomoshia@gmail.com>
2017-05-18 22:04:52 -0400

    JS COnditionals Exercises (before walkthorugh)
    commented out odd age lines bceause it wouldn't work with them in there

--- 3_Javascript/4_JS_Conditionals/bouncer.js
+++ 3_Javascript/4_JS_Conditionals/bouncer.js
@@ -1,14 +1,29 @@
 var age = prompt("How old are you?");
 
-if (age < 18) {
+if (age<0) {
+  console.log("Error. Try again.");
+  alert("Error. Try again.");
+}
+
+else if (age < 18) {
   console.log("Sorry, you are not old enough to enter the venue.");
 alert("Sorry, you are not old enough to enter the venue.");
 }
-// only runs if already established that age is not less than 18
+
 else if (age<21) {
   console.log("You can enter, but cannot drink");
   alert("You can enter, but cannot drink");
 }
+
+else if (age==21) {
+  console.log("Happy 21st birthday!! You get a free shot!");
+  alert("Happy 21st birthday!! You get a free shot!");
+}
+
+// else if (age% == 0) {
+//   console.log("your age is odd");
+//   alert("your age is odd");
+// }
 
 else {
   console.log("Come on in. You can drink");
