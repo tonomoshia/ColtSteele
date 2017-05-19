0e4b1419c2a3f79917f7076f715494af10d90272
Elizabeth Reiher <tonomoshia@gmail.com>
2017-05-18 22:10:14 -0400

    JS Conditionals Exercise (after solution)
    discovered age is odd language would not work beacuse a) it was wrong b) the other conditions could not co-exist with it.

--- 3_Javascript/4_JS_Conditionals/bouncer.js
+++ 3_Javascript/4_JS_Conditionals/bouncer.js
@@ -1,31 +1,33 @@
 var age = prompt("How old are you?");
 
+// if age is negative
 if (age<0) {
   console.log("Error. Try again.");
   alert("Error. Try again.");
 }
 
-else if (age < 18) {
-  console.log("Sorry, you are not old enough to enter the venue.");
-alert("Sorry, you are not old enough to enter the venue.");
-}
+// else if (age < 18) {
+//   console.log("Sorry, you are not old enough to enter the venue.");
+// alert("Sorry, you are not old enough to enter the venue.");
+// }
 
-else if (age<21) {
-  console.log("You can enter, but cannot drink");
-  alert("You can enter, but cannot drink");
-}
-
-else if (age==21) {
+// else if (age<21) {
+//   console.log("You can enter, but cannot drink");
+//   alert("You can enter, but cannot drink");
+// }
+// if age is 21
+if (age==21) {
   console.log("Happy 21st birthday!! You get a free shot!");
   alert("Happy 21st birthday!! You get a free shot!");
 }
 
-// else if (age% == 0) {
-//   console.log("your age is odd");
-//   alert("your age is odd");
-// }
-
-else {
-  console.log("Come on in. You can drink");
-  alert("Come on in. You can drink");
+// if age is odd
+if (age%2 !==0) {
+  console.log("Your age is odd!");
+  alert("Your age is odd!");
 }
+
+// else {
+//   console.log("Come on in. You can drink");
+//   alert("Come on in. You can drink");
+// }
