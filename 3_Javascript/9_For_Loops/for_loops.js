689895ee40d3cccede6ceab90e2709c79a48488d
Elizabeth Reiher <tonomoshia@gmail.com>
2017-05-21 23:41:03 -0400

    For Loops Intro

---
+++ 3_Javascript/9_For_Loops/for_loops.js
@@ -0,0 +1,14 @@
+// for_loops.js
+
+//prints out, starting with 0 and (not including) 16, numbers with 8 added to them, so only 0 and 8)
+for (var i = 0; i < 16; i +=8) {
+  console.log(i);
+}
+
+//prints out every other letter of the string, which is "hello"
+var str = "ahceclwlxo";
+
+for(var i = 1; i < str.length; i+=2) {
+  console.log(str[i]);
+}
+
